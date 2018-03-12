95aa2cf4f47065d7688f0e00b35967b2487fdf41
Daiki Hayakawa <daiki.hayakawa@cern.ch>
2018-03-13 00:02:17 +0100

    Update

--- calculateSNR.cxx
+++ calculateSNR.cxx
@@ -28,23 +28,25 @@ void calculateSNR(){
   TH2D* h_NoTOF = (TH2D*)file->Get("himage;1");
 
   const int i_fin=h_TOF->GetNbinsX();
+  // cout << "Number of bins: " << i_fin << endl;
 
-  // cout << "Total number of bins X: " << h_TOF  ->GetNbinsX() << " Y: " << h_TOF  ->GetNbinsY() << endl;
-  // cout << "Total number of bins X: " << h_NoTOF->GetNbinsX() << " Y: " << h_NoTOF->GetNbinsY() << endl;
-   
   Double_t sum_TOF; 
   Double_t sum_NoTOF; 
+  Int_t nbins=0;
   for(int ix=1; ix<=i_fin; ix++){
     for(int iy=1; iy<=i_fin; iy++){
-      if(IsROI(ix,iy,i_fin)) sum_TOF  +=(h_TOF  ->GetBinContent(ix,iy));
-      if(IsROI(ix,iy,i_fin)) sum_NoTOF+=(h_NoTOF->GetBinContent(ix,iy));
+      if(IsROI(ix,iy,i_fin)) {
+        sum_TOF  +=(h_TOF  ->GetBinContent(ix,iy));
+        sum_NoTOF+=(h_NoTOF->GetBinContent(ix,iy));
+        nbins++;
+      }
     }
   }
 
   // cout << "sum with TOF: "    << sum_TOF << endl;
   // cout << "sum without TOF: " << sum_NoTOF << endl;
-  Double_t mean_TOF   = sum_TOF  /(Double_t)(i_fin*i_fin);
-  Double_t mean_NoTOF = sum_NoTOF/(Double_t)(i_fin*i_fin);
+  Double_t mean_TOF   = sum_TOF  /(Double_t)nbins;
+  Double_t mean_NoTOF = sum_NoTOF/(Double_t)nbins;
   cout << "mean with TOF: "    << mean_TOF << endl;
   cout << "mean without TOF: " << mean_NoTOF << endl;
 
@@ -52,14 +54,16 @@ void calculateSNR(){
   Double_t sd2_NoTOF;
   for(int ix=1; ix<=i_fin; ix++){
     for(int iy=1; iy<=i_fin; iy++){
-      if(IsROI(ix,iy,i_fin)) sd2_TOF   += pow((h_TOF  ->GetBinContent(ix,iy)-mean_TOF),  2);
-      if(IsROI(ix,iy,i_fin)) sd2_NoTOF += pow((h_NoTOF->GetBinContent(ix,iy)-mean_NoTOF),2);
+      if(IsROI(ix,iy,i_fin)) {
+        sd2_TOF   += pow((h_TOF  ->GetBinContent(ix,iy)-mean_TOF),  2);
+        sd2_NoTOF += pow((h_NoTOF->GetBinContent(ix,iy)-mean_NoTOF),2);
+      }
     }
   }
   // cout << "sd2 with TOF: " << sd2_TOF << endl;
   // cout << "sd2 without TOF: " << sd2_NoTOF << endl;
-  Double_t sd_TOF  =sqrt(sd2_TOF  /(Double_t)(i_fin*i_fin));
-  Double_t sd_NoTOF=sqrt(sd2_NoTOF/(Double_t)(i_fin*i_fin));
+  Double_t sd_TOF  =sqrt(sd2_TOF  /(Double_t)nbins);
+  Double_t sd_NoTOF=sqrt(sd2_NoTOF/(Double_t)nbins);
   cout << "sd with TOF: " << sd_TOF << endl;
   cout << "sd without TOF: " << sd_NoTOF << endl;
   Double_t snr_TOF  = mean_TOF  /sd_TOF;
